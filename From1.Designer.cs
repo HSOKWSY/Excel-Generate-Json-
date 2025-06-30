fb32cb87f6c5cc1aeac7a99cff3b061cee0f66bb
HSOKWSY <2580592191@qq.com>
2025-06-30 11:00:09 +0800

    Add files via upload

---
+++ From1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Excel_Json
+{
+    partial class From1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.Convertbutton = new System.Windows.Forms.Button();
+            this.ColtextBox = new System.Windows.Forms.TextBox();
+            this.Colbutton = new System.Windows.Forms.Button();
+            this.Rowbutton = new System.Windows.Forms.Button();
+            this.RowtextBox = new System.Windows.Forms.TextBox();
+            this.SavetextBox = new System.Windows.Forms.TextBox();
+            this.Savebutton = new System.Windows.Forms.Button();
+            this.ShowtextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonSelectFile.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonSelectFile.Location = new System.Drawing.Point(1194, 68);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(200, 50);
+            this.buttonSelectFile.TabIndex = 0;
+            this.buttonSelectFile.Text = "打开文件";
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxFilePath.Location = new System.Drawing.Point(107, 68);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(1055, 50);
+            this.textBoxFilePath.TabIndex = 1;
+            // 
+            // Convertbutton
+            // 
+            this.Convertbutton.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Convertbutton.Location = new System.Drawing.Point(1194, 809);
+            this.Convertbutton.Name = "Convertbutton";
+            this.Convertbutton.Size = new System.Drawing.Size(200, 50);
+            this.Convertbutton.TabIndex = 2;
+            this.Convertbutton.Text = "转化";
+            this.Convertbutton.UseVisualStyleBackColor = true;
+            this.Convertbutton.Click += new System.EventHandler(this.Convertbutton_Click);
+            // 
+            // ColtextBox
+            // 
+            this.ColtextBox.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ColtextBox.Location = new System.Drawing.Point(107, 159);
+            this.ColtextBox.Name = "ColtextBox";
+            this.ColtextBox.Size = new System.Drawing.Size(425, 50);
+            this.ColtextBox.TabIndex = 3;
+            this.ColtextBox.Text = "变量名的列(如B请输入2)";
+            // 
+            // Colbutton
+            // 
+            this.Colbutton.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Colbutton.Location = new System.Drawing.Point(557, 159);
+            this.Colbutton.Name = "Colbutton";
+            this.Colbutton.Size = new System.Drawing.Size(167, 54);
+            this.Colbutton.TabIndex = 4;
+            this.Colbutton.Text = "确定COL";
+            this.Colbutton.UseVisualStyleBackColor = true;
+            this.Colbutton.Click += new System.EventHandler(this.Colbutton_Click);
+            // 
+            // Rowbutton
+            // 
+            this.Rowbutton.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Rowbutton.Location = new System.Drawing.Point(1194, 163);
+            this.Rowbutton.Name = "Rowbutton";
+            this.Rowbutton.Size = new System.Drawing.Size(200, 50);
+            this.Rowbutton.TabIndex = 6;
+            this.Rowbutton.Text = "确定ROW";
+            this.Rowbutton.UseVisualStyleBackColor = true;
+            this.Rowbutton.Click += new System.EventHandler(this.Rowbutton_Click);
+            // 
+            // RowtextBox
+            // 
+            this.RowtextBox.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.RowtextBox.Location = new System.Drawing.Point(775, 163);
+            this.RowtextBox.Name = "RowtextBox";
+            this.RowtextBox.Size = new System.Drawing.Size(387, 50);
+            this.RowtextBox.TabIndex = 5;
+            this.RowtextBox.Text = "从第几行开始";
+            // 
+            // SavetextBox
+            // 
+            this.SavetextBox.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SavetextBox.Location = new System.Drawing.Point(107, 248);
+            this.SavetextBox.Name = "SavetextBox";
+            this.SavetextBox.Size = new System.Drawing.Size(1055, 50);
+            this.SavetextBox.TabIndex = 7;
+            // 
+            // Savebutton
+            // 
+            this.Savebutton.Font = new System.Drawing.Font("宋体", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Savebutton.Location = new System.Drawing.Point(1194, 248);
+            this.Savebutton.Name = "Savebutton";
+            this.Savebutton.Size = new System.Drawing.Size(200, 50);
+            this.Savebutton.TabIndex = 8;
+            this.Savebutton.Text = "选择路径";
+            this.Savebutton.UseVisualStyleBackColor = true;
+            this.Savebutton.Click += new System.EventHandler(this.Savebutton_Click);
+            // 
+            // ShowtextBox
+            // 
+            this.ShowtextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ShowtextBox.Location = new System.Drawing.Point(107, 344);
+            this.ShowtextBox.Multiline = true;
+            this.ShowtextBox.Name = "ShowtextBox";
+            this.ShowtextBox.Size = new System.Drawing.Size(1055, 515);
+            this.ShowtextBox.TabIndex = 9;
+            // 
+            // From1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1496, 974);
+            this.Controls.Add(this.ShowtextBox);
+            this.Controls.Add(this.Savebutton);
+            this.Controls.Add(this.SavetextBox);
+            this.Controls.Add(this.Rowbutton);
+            this.Controls.Add(this.RowtextBox);
+            this.Controls.Add(this.Colbutton);
+            this.Controls.Add(this.ColtextBox);
+            this.Controls.Add(this.Convertbutton);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.buttonSelectFile);
+            this.Name = "From1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button Convertbutton;
+        private System.Windows.Forms.TextBox ColtextBox;
+        private System.Windows.Forms.Button Colbutton;
+        private System.Windows.Forms.Button Rowbutton;
+        private System.Windows.Forms.TextBox RowtextBox;
+        private System.Windows.Forms.TextBox SavetextBox;
+        private System.Windows.Forms.Button Savebutton;
+        private System.Windows.Forms.TextBox ShowtextBox;
+    }
+}
+
---
+++ From1.cs
@@ -0,0 +1,185 @@
+﻿using Newtonsoft.Json;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace Excel_Json
+{
+    public partial class From1 : Form
+    {
+        private string filePath = string.Empty;// 全局变量，用于保存选择的文件路径
+        private string selectedFolderPath;  // 保存选择的文件夹路径
+        private int Col;  // 保存用户输入的列数
+        private int Row;  // 保存用户输入的行数
+        public From1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSelectFile_Click(object sender, EventArgs e)
+        {
+            // 创建 OpenFileDialog 对象
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // 设置打开文件对话框的初始目录
+            openFileDialog.InitialDirectory = "c:\\";
+            // 设置文件过滤器
+            openFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            // 显示对话框并判断用户是否选择了文件
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // 获取文件路径并赋值给全局变量 filePath
+                filePath = openFileDialog.FileName;
+
+                // 在文本框或标签上显示文件路径
+                textBoxFilePath.Text = filePath;
+            }
+        }
+
+        private void Convertbutton_Click(object sender, EventArgs e)
+        {
+            // 检查用户是否选择了文件
+            if (string.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("请先选择一个 Excel 文件！");
+                return;
+            }
+
+            // 设置 EPPlus 的 LicenseContext
+            OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+            // 打开 Excel 文件
+            var fileInfo = new FileInfo(filePath);  // 使用用户选择的文件路径
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                var worksheet = package.Workbook.Worksheets[0];
+
+                // 创建一个字典来保存转换后的数据
+                var jsonDict = new Dictionary<string, Dictionary<string, object>>();
+
+                int startColumn = 2;  // 第二列开始读取
+
+                // 遍历所有行
+                for (int row = Row; row <= worksheet.Dimension.End.Row; row++)
+                {
+                    // 第二列作为变量名，删除前三个字母
+                    string variableName = worksheet.Cells[row,Col].Text;
+                    if (variableName.Length > 3)
+                    {
+                        variableName = variableName.Substring(3);  // 从第四个字符开始
+                    }
+
+                    // 创建一个字典来保存属性
+                    var attributes = new Dictionary<string, object>();
+
+                    // 遍历其他列（从第三列开始），将其作为属性
+                    for (int col = startColumn + 1; col <= worksheet.Dimension.End.Column; col++)
+                    {
+                        // 读取属性名和属性值（假设上一行为属性名）
+                        string attributeName = worksheet.Cells[Row - 1, col].Text;  // 属性名在第一行
+                        string attributeValue = worksheet.Cells[row, col].Text;
+
+                        // 将属性添加到字典中
+                        attributes[attributeName] = attributeValue;
+                    }
+
+                    // 将属性字典添加到主字典中，以变量名为键
+                    jsonDict[variableName] = attributes;
+                }
+
+                // 将字典转换为 JSON 格式
+                string jsonString = JsonConvert.SerializeObject(jsonDict, Formatting.Indented);
+
+                // 输出 JSON 字符串
+                Console.WriteLine(jsonString);
+
+                // 在 TextBox 中预览 JSON 内容
+                ShowtextBox.Text = jsonString;
+
+                // 拼接完整的文件路径（将文件名和文件夹路径结合起来）
+                string filePath = Path.Combine(selectedFolderPath, "output.json");
+
+                // 保存 JSON 字符串到文件
+                File.WriteAllText(filePath, jsonString);
+
+                // 通知用户转换完成
+                MessageBox.Show("Excel 文件已成功转换为 JSON 文件！");
+            }
+        }
+
+        private void Colbutton_Click(object sender, EventArgs e)
+        {
+            // 验证输入是否为有效数字
+            if (int.TryParse(ColtextBox.Text, out int result))
+            {
+                // 将数字保存到变量
+                Col = result;
+
+                // 显示保存成功的信息
+                MessageBox.Show("输入的数字已保存: " + Col);
+            }
+            else
+            {
+                // 如果输入的不是数字，提示用户
+                MessageBox.Show("请输入有效的数字！");
+            }
+        }
+
+        private void Rowbutton_Click(object sender, EventArgs e)
+        {
+            // 验证输入是否为有效数字
+            if (int.TryParse(RowtextBox.Text, out int result))
+            {
+                // 将数字保存到变量
+                Row = result;
+
+                // 显示保存成功的信息
+                MessageBox.Show("输入的数字已保存: " + Row);
+            }
+            else
+            {
+                // 如果输入的不是数字，提示用户
+                MessageBox.Show("请输入有效的数字！");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Savebutton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.Description = "请选择保存文件夹";
+                folderDialog.ShowNewFolderButton = true;  // 允许用户新建文件夹
+
+                // 显示对话框并判断用户是否选择了文件夹
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // 获取文件夹路径并保存
+                    selectedFolderPath = folderDialog.SelectedPath;
+
+                    // 在文本框或标签上显示文件夹路径
+                    SavetextBox.Text = selectedFolderPath;
+                }
+            }
+        }
+    }
+}
